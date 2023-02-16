c01110ec72767f0721700bc861df5500947048b6
projectmanager@jpassetmanagement.com <projectmanager@jpassetmanagement.com>
2023-02-16 15:31:14 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "guest-experience-38923",
   url: "https://guest-experience-38923.botics.co",
   api: "https://guest-experience-38923.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
